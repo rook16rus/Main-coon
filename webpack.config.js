7c3274026948f633db9560a16bb1f83f74222cd6
rook16rus <kamalov00albert@gmail.com>
2021-07-27 13:42:56 +0300

    Подготовил сборщик проекта Webpack. Сверстал шапку для мобильных экранов

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "scss-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "scss-loader", "sass-loader"],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/,
+                type: "asset/resource",
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+        ]
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
